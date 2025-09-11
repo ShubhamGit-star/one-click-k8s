e5793ab92c0c31dffa304e870a4e3cb7102e62ab
ShubhamGit-star <guptashubhammeerut@gmail.com>
2025-09-11 17:17:33 +0530

    Delete terraform/backend.tf

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
- backend "s3" {
-    bucket         = "k8s-shubham-tf-state"      
-    key            = "terraform.tfstate"          
-    region         = "us-east-1"                   
-    dynamodb_table = "k8s-shubham-tf-lock"        
-    encrypt        = true                           
-  }
-}
