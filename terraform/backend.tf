96658608c5df17e452f9a196a4fbdaf94988ef94
ShubhamGit-star <guptashubhammeerut@gmail.com>
2025-09-11 16:07:06 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
-#terraform {
-# backend "s3" {
-#    bucket         = "k8s-shubham-tf-state"      
-#    key            = "terraform.tfstate"          
-#    region         = "us-east-1"                   
-#    dynamodb_table = "k8s-shubham-tf-lock"        
-#    encrypt        = true                           
-#  }
-#}
+terraform {
+ backend "s3" {
+    bucket         = "k8s-shubham-tf-state"      
+    key            = "terraform.tfstate"          
+    region         = "us-east-1"                   
+    dynamodb_table = "k8s-shubham-tf-lock"        
+    encrypt        = true                           
+  }
+}
